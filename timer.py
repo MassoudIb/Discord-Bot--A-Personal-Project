60ed076f0c4a3fd387c1fc8a07d14a4b35aa66e4
MassoudIb <massoud.ibrahim@polymtl.ca>
2024-06-05 00:16:46 -0400

    Added Timer class
    For countdown functionality and inactivity timing

---
+++ timer.py
@@ -0,0 +1,32 @@
+import asyncio
+from datetime import datetime, timedelta
+import logging
+
+class Timer:
+    def __init__(self):
+        self.conversations = {}
+
+    async def end_conversation(self, user_id, guild_id, channel):
+        await asyncio.sleep(120)
+        if (user_id, guild_id) in self.conversations:
+            last_interaction_time = self.conversations[(user_id, guild_id)]['last_interaction']
+            if datetime.utcnow() - last_interaction_time >= timedelta(minutes=2):
+                del self.conversations[(user_id, guild_id)]
+                await channel.send("Ending conversation due to inactivity <:cat_evil:1158210003501596713>")
+                logging.info(f"Ended conversation with {user_id} in guild {guild_id} due to inactivity.")
+
+    async def countdown(self, channel, seconds):
+        hourglass_emoji = ':hourglass_flowing_sand:'
+        word_emojis = [':one:', ':two:', ':three:', ':four:', ':five:', ':six:', ':seven:', ':eight:', ':nine:', ':zero:']
+
+        message = await channel.send(f"{hourglass_emoji} Countdown: {seconds} seconds")
+        for i in range(seconds, 0, -1):
+            remaining_time = f"{hourglass_emoji} "
+            for digit in str(i):
+                if digit.isdigit():
+                    emoji_index = int(digit) - 1
+                    remaining_time += word_emojis[emoji_index]
+            await asyncio.sleep(1)
+            await message.edit(content=remaining_time)
+        await message.edit(content=f"# :hourglass: Countdown finished!")
+
